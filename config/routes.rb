9d869233e42cffb84eec3382ca0b0ce611cfbafa
dchuong09 <dchuong09@yahoo.com>
2018-04-20 11:06:14 -0700

    created user profile

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
 	get '/users/new', to: 'users#new', as: 'new_user'
 	post '/users', to: 'users#create'
-	
+	get '/users/:id', to: 'users#show', as: 'user'
 
 end
 #  root GET  /                   users#index
